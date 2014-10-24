e7e12c2a3836f6d08749b65a11d29a156d26c54b
Fabian Pflug <mail@gummu.de>
2014-10-24 10:36:45 +0200

    Die DataDrawer etwas verändert.
    Image und Graphic werden nun nicht mehr herum gereicht.
    Die main neu geschrieben, dass sie automatisiert für alle Datensätze ein Bild erstellt.

--- SpaceUsageRules/src/de/uni_hannover/spaceusagerules/core/DataDrawer.java
+++ SpaceUsageRules/src/de/uni_hannover/spaceusagerules/core/DataDrawer.java
@@ -6,8 +6,13 @@ import java.awt.Graphics;
 import java.awt.Polygon;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Vector;
 
@@ -26,17 +31,37 @@ public class DataDrawer {
 	private int width, height;
 	private int margin = 5;
 	private String format = "png";
-	
+	private Graphics gr;
+	private BufferedImage image;
 	
 	public DataDrawer(int width, int height, float radius){
-		OSM.useBuffer(true);
 		this.width = width;
 		this.height = height;
 		this.radius = radius;
+
 		
+		//Bild erstellen
+		image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		gr = image.getGraphics();
+		
+		//adjust font size
+		Font font = gr.getFont();
+		gr.setFont(font.deriveFont(9f));
+		
+		//Hintergrungd zeichnen
+		gr.setColor(Color.orange);
+		gr.fillRect(0, 0, width, height);
 	}
 	
-	public void drawWay(Graphics gr, Way w){
+	public BufferedImage getImage() {
+		return image;
+	}
+	
+	
+	public void drawWay(Way w){
+		drawWay(w, Color.black);
+	}
+	public void drawWay(Way w, Color color) {
 		
 		//create drawable polygon
 		int[] ints;
@@ -49,14 +74,14 @@ public class DataDrawer {
 		//draw and fill polygon
 //		gr.setColor(new Color(w.getFillColor()));
 //		gr.fillPolygon(polygon);
-		gr.setColor(Color.black);
+		gr.setColor(color);
 		gr.drawPolygon(polygon);
 		
 		//draw center (of bounding box)
 		Rectangle boundingBox = polygon.getBounds();
 		int centerX = boundingBox.width/2 + boundingBox.x;
 		int centerY = boundingBox.height/2 + boundingBox.y;
-		gr.setColor(Color.black);
+		gr.setColor(color);
 		gr.drawLine(centerX-3, centerY, centerX+3, centerY);
 		gr.drawLine(centerX, centerY-3, centerX, centerY+3);
 		
@@ -67,7 +92,7 @@ public class DataDrawer {
 			tag.add(key+"->"+w.getTags().get(key));
 		}
 //		tag = filterTags(tag);
-		drawTags(gr,tag,centerX,centerY);
+		drawTags(tag,centerX,centerY);
 		
 	}
 	
@@ -85,14 +110,10 @@ public class DataDrawer {
 	}
 	
 	
-	private void drawTags(Graphics gr, List<String> tags, int x, int y){
-		
+	private void drawTags(List<String> tags, int x, int y){
 		for(int i=0;i<tags.size();i++){
 			gr.drawString(tags.get(i), x, y+(i+1)*11);
 		}
-		
-		
-		
 	}
 	
 	
@@ -119,7 +140,7 @@ public class DataDrawer {
 		return output;
 	}
 	
-	public BufferedImage render(Coordinate p){
+	public void render(Coordinate p){
 		
 		List<Way> data = retrieveData(p);
 		
@@ -137,22 +158,10 @@ public class DataDrawer {
 			}
 		}
 		
-		//Bild erstellen
-		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-		Graphics gr = image.getGraphics();
-		
-		//adjust font size
-		Font font = gr.getFont();
-		gr.setFont(font.deriveFont(9f));
-		
-		//Hintergrungd zeichnen
-		gr.setColor(Color.orange);
-		gr.fillRect(0, 0, width, height);
-		
 		//polygone zeichnen
 		for(Way w : data){
 			if(w.isArea())
-				drawWay(gr, w);
+				drawWay(w);
 		}
 		
 		//draw reference point p
@@ -161,12 +170,10 @@ public class DataDrawer {
 		gr.drawOval(ref[0]-6, ref[1]-6, 12, 12);
 		gr.fillOval(ref[0]-5, ref[1]-5, 10, 10);
 		
-		
-		return image;
 	}
 	
 	
-	public void saveImage(String name, BufferedImage image) throws IOException
+	public void saveImage(String name) throws IOException
 	{
 		
 		//TODO optional: create metadata
@@ -185,15 +192,40 @@ public class DataDrawer {
 	}
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		OSM.useBuffer(true);
 		
+		
+		File f = new File("../SpaceUsageRulesVis/assets/Data.txt");
+		BufferedReader br = new BufferedReader(new FileReader(f));
+		String line;
+		br.readLine();
+		String oldID = null;
+		while((line = br.readLine()) != null) {
+			String[] bla = line.split(",");
+			String id = bla[0].trim();
+			if(id.equals(oldID))
+				continue;
+			oldID=id;
+			System.out.println("Bearbeite " + id);
+			InputStream is = new FileInputStream(new File("../SpaceUsageRulesVis/assets/" + id + ".jpg"));
+			DataDrawer drawer = new DataDrawer(5000,5000, 0.0005f);
+			drawer.render(Image.readCoordinates(is));
+			Way right = new Way();
+			right.addAllCoordinates(KML.loadKML(new File("../SpaceUsageRulesVis/assets/" + id + ".truth.kml")).getPoints());
+			drawer.drawWay(right,Color.green);
+			drawer.saveImage("images/" + id + ".png");
+		}
+		br.close();
+		
+		/*
 		DataDrawer drawer = new DataDrawer(10000,5000, 0.0005f);
 		BufferedImage image = drawer.render(new Coordinate(50.9304, 5.33901));
 		try {
 			drawer.saveImage("001.png", image);
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
+		} // */
 
 	}
 
